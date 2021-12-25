b5c8c2bb2493d24afefe17367a35f3b23d50c9db
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-12-25 18:37:19 +0100

    Update server.js

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -4,3 +4,16 @@ handle GET request on '/' and send 'Welcome to homepage' in response in plain te
 handle GET request on '/about' and send response 'this is all about NodeJS' using h2 tag in HTML.
 handle POST request on '/about' and send json response {message: this is a post request}.
 
+var http = require('http')
+var server = http.createServer(handleRequest)
+server.listen(5000)
+
+function handleRequest(req,res){
+  if(req.method === 'GET' && req.url === '/'){
+    res.write('Welcome to homepage')
+  } else if(req.method === 'GET' && req.url === '/about'){
+    res.write('this is all about NodeJS')
+  } else if(req.method === 'POST' && req.url === '/about'){
+    res.write('message:this is a post request').json()
+  }
+}
