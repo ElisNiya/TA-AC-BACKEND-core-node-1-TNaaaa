84bd40e6f08c5682523796b02240f58d4a86761d
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-12-25 17:17:56 +0100

    Update server.js

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,7 +3,13 @@ Add statusCode of 201 and headers to send html content to response
 Do request on server using different HTTP methods using Postman and write code to check request methods for multiple requests.
 
 var http = require('http')
-var server = http.createServer(req, res);
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+  res.statusCode = 201;
+  res.setHeader('content', 'text/html')
+  res.end('<h2>Welcome</h2>)
+}
 server.listen(4444);
-res.statusCode = 201;
-res.setHeader('content', 'text/html')
+
+
