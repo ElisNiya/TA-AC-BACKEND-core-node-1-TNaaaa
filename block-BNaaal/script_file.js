a8c573e56635e3dfe2824d523136de7bdae64512
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-12-25 15:45:46 +0100

    Update script_file.js

--- block-BNaaal/script_file.js
+++ block-BNaaal/script_file.js
@@ -1,3 +1,4 @@
-var server = http.createServer(request, response)
+var http = require('http');
+var server = http.createServer(request, response);
 
-server.listen(4000,'localhost')
+server.listen(4000,'localhost');
