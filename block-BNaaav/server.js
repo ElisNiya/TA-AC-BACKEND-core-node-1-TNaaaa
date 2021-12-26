71bd4d2724931347534a1624798067a3ab13a97e
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-12-26 14:53:57 +0100

    Update server.js

--- block-BNaaav/server.js
+++ block-BNaaav/server.js
@@ -14,11 +14,40 @@ img1.jpg
 js(directory)
 app.js
 ...other js files
+
 Instructions
 create seperate routes for seperate html templates to render for example:-
 "/" -> render index.html
 "/about" -> render about.html
 handle requests for all css files in a single seperate router for example:-
+  
+  function handleRequest(req, res){
+  if(req.method === 'GET' && req.url === '/') {
+        res.setheader('content-type', 'text/plain')
+        res.end('Suraj')
+  } else if (req.method === 'GET' && req.url === '/stream'){
+       res.setheader('content-type', 'text/html')
+        res.end(<h2>Suraj</h2>)
+    fs.createReadStream('./node.html').pipe(res)
+  } else {
+    res.statusCode = 404;
+    res.end('page not found')
+  }
+  
+
+server.listen(PORT, () => {
+  console.log('server listening on port' + PORT)
+})
+
+   
+  
+  fs.readFile('./node.html', (err,content)=> {
+       if(err) console.log(err)         
+       res.setHeader('content-type', 'text/html')
+      res.end(content);
+    })
+
+  
 // check for css requests using url
 if (req.url.split(".").pop() === "css") {
   // set header for css file
